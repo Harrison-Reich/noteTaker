e9f12dced14b1c5e3e28c840b040e9cec727fec3
Harrison Reich <hreich1998@gmail.com>
2021-12-13 15:23:36 -0800

    sending data to index page

--- server.js
+++ server.js
@@ -13,4 +13,8 @@ app.get('/notes', (req,res) => {
   res.sendFile(path.join(__dirname, 'public', 'notes.html'))
 })
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
 app.listen(3000)
